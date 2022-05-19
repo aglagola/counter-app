c9a65773500a84463dcb87537b9b85943a878510
Adam Glagola <adam.m.glagola@gmail.com>
2022-05-18 22:39:41 -0400

    lifted state

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
-
+// Stateless Functional Components, can use just a function
 class NavBar extends Component {
   render() {
     return (
       <nav class="navbar bg-light">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">
-            Navbar
+            Navbar{" "}
+            <span className="badge badge-pill badge-secondary text-success">
+              {this.props.totalCounter}
+            </span>
           </a>
         </div>
       </nav>
