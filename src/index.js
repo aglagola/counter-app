37cddf69a49db3029b2472288c2316146860cf9e
Adam Glagola <adam.m.glagola@gmail.com>
2022-05-18 20:26:09 -0400

    added counter, couters, navbar components

--- src/index.js
+++ src/index.js
@@ -5,12 +5,11 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
-import Counters from "./components/counters";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Counters />
+    <App />
   </React.StrictMode>
 );
 
